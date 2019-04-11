ed8a7e84360e0e0619ce8daebb8dc89bde924577
Justin Thwaites <Justin>
2019-04-10 20:53:30 -0600

    This is a full setup of the classes that we will need

---
+++ battleship.hpp
@@ -0,0 +1,87 @@
+#ifndef BATTLESHIP
+#define BATTLESHIP
+
+#include <string>
+
+#include <vector>
+#include <queue>
+#include<iostream>
+#include<fstream>
+#include<sstream>
+#include<string>
+#include<ctime>
+
+struct ships{
+  int turnstatus;
+  bool bship;
+  int size;
+  int strength;
+  int player;
+
+};
+
+struct area{
+  int ir;
+  int ic;
+  int or;
+  int oc;
+//specifies area on the board by top left and bottom right corners
+};
+
+
+class PriorityQueue{
+    public:
+    PriorityQueue(int queueSize);
+    ~PriorityQueue();
+    void enqueue (ships* a);
+    void dequeue();
+    ships* peek();
+    bool isFull();
+    bool isEmpty();
+    void lowerpriority();
+    private:
+    //Purpose: maintain heap properties by swapping node with parent if necessary
+    void repairUpward(int nodeIndex);
+    void repairDownward(int nodeIndex);
+
+    ships** priorityQueue; //pointer to the array used to implement priority queue
+    int currentQueueSize; //number of group currently in the priority queue
+    int maxQueueSize; //maximum capacity of priority queue
+};
+
+
+
+
+class Battleship {
+  public:
+    Battleship(int players);
+    ~Battleship();
+    void showall();
+    void printboard(area a);
+    void addship(ships *boat, area r);
+    void damage(ships *boat, int damage);
+    void removeship(ships *boat);
+    bool shipcollide(area r);
+    int moveship(ships *boat, area iregion, area oregion );
+    bool attack(area a);
+    void peek(area a);
+    ships* lookup();
+
+  private:
+    /* member functions */
+    void setboard();
+    void setboat(area a, ships *boat);
+    void order(int order[]);
+    void createboard(int segment);
+    bool isEmptySegment(int segment);
+
+    PriorityQueue turn;
+    ships*** board;
+    ships* ship;
+    int playerNum;
+    const int scale=14;
+};
+
+
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,3 +1,22 @@
+#include <vector>
+#include <queue>
+#include<iostream>
+#include<fstream>
+#include<sstream>
+#include<string>
+#include<ctime>
+
+#include "battleship.hpp"
+
+
+
 using namespace std;
 
 int main(int argc, char* argv[]){
+  int numb;
+  cout<< "Enter the number of Players:";
+  cin>>numb;
+  Battleship game(numb);
+  game.showall();
+  return 0;
+}
